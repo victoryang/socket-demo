77a4930e82f03a4bf2d2e49084fe19e7c932e716
novell <novell@linux.suse>
2017-08-15 00:01:59 +0800

    add decode.go

--- Go/decode.go
+++ Go/decode.go
@@ -13,11 +13,21 @@ type kvs struct{
 
 
 func main(){
+	var kv []kvs
 	file, _ := os.Open("mrtmp.job-2-2")
 	dec := json.NewDecoder(file)
 	err := dec.Decode(&kv)
 	if err == nil {
 		fmt.Println(kv)
 	}
+
+	m := make(map[string][]string)
+	s := []string{"a","b","c"}
+	m["1"] = s
+	fmt.Println(m["1"])
+	/*for err == nil {
+		fmt.Println(kv)
+		err = dec.Decode(&kv)
+	}*/
 	
 }
