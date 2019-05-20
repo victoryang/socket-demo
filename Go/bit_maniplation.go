28b7ecaceecff68076206eb871142e2b680508b3
victoryang <yangtao3601@126.com>
2019-05-20 11:50:55 +0800

    add bit_manipulation

--- Go/bit_maniplation.go
+++ Go/bit_maniplation.go
@@ -27,9 +27,23 @@ func find_one_nums(num int) int {
     return i
 }
 
+func set_bit(num *int, index int) {
+    *num |= 1 << uint(index)
+}
+
+func unset_bit(num *int, index int) {
+    *num &^= 1 << uint(index)
+}
+
 func main() {
     var num int = 8
 
     fmt.Println("last zero's numbers: ", last_zero_nums(num))
     fmt.Println("one's numbers: ", find_one_nums(num))
+
+    set_bit(&num, 2)
+    fmt.Println("set bit", 2, ": ", num)
+
+    unset_bit(&num, 2)
+    fmt.Println("unset bit", 2, ": ", num)
 }
