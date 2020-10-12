6ad992c627ae4dcb17dc3af382d0606f73ede1f1
yangtao <yangtao@xueqiu.com>
2020-10-12 11:23:24 +0800

    leetcode 0002

--- Go/leetcode/leetcode-0002.go
+++ Go/leetcode/leetcode-0002.go
@@ -1,87 +1,95 @@
 package main
 
-// Add Two Number
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. 
+
+ * The digits are stored in reverse order, and each of their nodes contains a single digit. 
+
+ * Add the two numbers and return the sum as a linked list.
+
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ */
 
 import "fmt"
 
-type Node struct {
-	val 	int
-	next 	*Node
+/** Definition for singly-linked list.
+ *  type ListNode struct {
+ *      Val int
+ *      Next *ListNode
+ *  }
+ */
+
+type ListNode struct {
+	Val 	int
+	Next 	*ListNode
 }
 
-func make_list(arr []int) *Node {
-	var list = new(Node)
-	list.val = 0
-	list.next = nil
+func make_list(num int) *ListNode {
+    var list *ListNode = nil
 
-	for _,v := range arr {
-		tmp := new(Node)
-		tmp.val = v
-		tmp.next = list.next
-		list.next = tmp
-	}
+    n := num
+    for n>0 {
+        t := n % 10
+        tmp := new(ListNode)
+        tmp.Val = t
+        tmp.Next = list
+        list = tmp
+        n = n / 10
+    }
 
-	return list
+    return list
 }
 
-func print_list(list *Node){
-	var t *Node = list.next
+func print_list(list *ListNode){
+    var t *ListNode = list
 
-	fmt.Print("List:")
-	for t!=nil {
-		fmt.Print(" ", t.val)
-		t = t.next
-	}
-	fmt.Print("\n")
+    for t!=nil {
+        fmt.Print(" ", t.Val)
+        t = t.Next
+    }
+    fmt.Print("\n")
 }
 
-func add_two_numbers(a,b *Node) *Node {
-	var pa = a.next
-	var pb = b.next
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    t1 := l1
+    t2 := l2
+    sum := 0
+    unit := 1
+    for t1!=nil && t2!=nil {
+        sum = sum + (t1.Val + t2.Val)*unit
+        unit = unit * 10
 
-	var pc = new(Node)
-	pc.val = 0
-	pc.next = nil
-	var c *Node = pc
+        t1 = t1.Next
+        t2 = t2.Next
+    }
 
-	var carry = 0
-	for pa != nil || pb != nil {
-		var x int
-		if pa != nil {
-			x = pa.val
-		}
+    for t1!=nil {
+        sum = sum + t1.Val*unit
+        unit = unit * 10
 
-		var y int
-		if pb != nil {
-			y = pb.val
-		}
+        t1 = t1.Next
+    }
 
-		sum := carry + x + y
-		carry = sum / 10
+    for t2!=nil {
+        sum = sum + t2.Val*unit
+        unit = unit * 10
 
-		tmp := new(Node)
-		tmp.val = sum % 10
-		tmp.next = c.next
-		c.next = tmp
-		c = tmp
+        t2 = t2.Next
+    } 
 
-		pa = pa.next
-		pb = pb.next
-	}
-
-	return pc
+    return make_list(sum)
 }
 
 func main() {
-	a := [3]int{3,4,2}
-	b := [3]int{4,6,5}
+    a := 9999999
+    b := 9999
+    c := make_list(a)
+    d := make_list(b)
 
-	c := make_list(a[:])
-	d := make_list(b[:])
+    print_list(c)
+    print_list(d)
 
-	print_list(c)
-	print_list(d)
-
-	e := add_two_numbers(c, d)
-	print_list(e)
+    e := addTwoNumbers(c, d)
+    print_list(e)
 }
